49025f799e2028709589c0a2b3a3e25696b54aa0
Rafal Bartkowski <scorpionikr@gmail.com>
2020-03-23 16:34:01 +0100

    Wersja responsywna strony

---
+++ js/app.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const hamburger = document.querySelector('.hamburger button');
+    const menu = document.querySelector('.list__navigation');
+    const menuup = document.querySelector('.page-header-nav');
+    hamburger.addEventListener('click', function(){
+        menu.classList.toggle('visible');
+        menu.classList.toggle('unvisible');
+    });
+    const mobile = window.matchMedia("screen and (max-width: 767px)");
+    mobile.addListener(function(){
+        if (mobile.matches) {
+            menu.classList.remove('visible');
+        }
+    });
+});
