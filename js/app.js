da265d9e4d9c66ceebf4a3e78de2f28e31fc0b3a
Rafal Bartkowski <scorpionikr@gmail.com>
2020-04-10 15:27:57 +0200

    Poprawione wyswietlanie i zrobione menu

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const hamburger = document.querySelector('.hamburger button');
     const menu = document.querySelector('.list__navigation');
     const menuup = document.querySelector('.page-header-nav');
+    const menu_list = document.querySelectorAll('ul > li ');
     hamburger.addEventListener('click', function(){
         menu.classList.toggle('visible');
         menu.classList.toggle('unvisible');
@@ -11,5 +12,12 @@ document.addEventListener("DOMContentLoaded", function () {
         if (mobile.matches) {
             menu.classList.remove('visible');
         }
+    });
+
+    menu_list.forEach(function(element) {
+        element.addEventListener('click', function(){
+            menu.classList.toggle('visible');
+            menu.classList.toggle('unvisible');
+        });
     });
 });
